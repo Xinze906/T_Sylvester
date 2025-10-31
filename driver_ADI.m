05b1035a5fef8a441a2bb06e6b0f4146cee79cb2
LordofCarrots906 <renxinze@hotmail.com>
2025-10-31 16:45:18 -0700

    Use and compare ADI and fADI on small Sylvester equation
    just to learn how ADI works, not related to T-Sylvester

---
+++ driver_ADI.m
@@ -0,0 +1,39 @@
+% Deriver code for 
+% ADI for solving matrix equation AX - XB = C for small matrix
+% and compare its performance with fADI
+
+%% 
+% initialize matrix A, B
+n = 100;
+a = 1; b = 4;  % interval for eigs of A is [a, b]
+Aeigs = (b-a)*rand(n,1) + a; 
+A = diag(Aeigs);
+B = -A; 
+Beigs = diag(B);% B is reflection of A across imaginary axis
+
+% initialize right-hand-side matrix C = GF' of rank rank_C
+rank_C = 3;
+G = rand(n,rank_C); F = rand(n,rank_C); 
+C = G*F';
+
+%%
+% get shift paramters
+% for k steps of ADI or fADI
+k = 12; 
+I = [[a,b], [-b, -a]];
+[p, q] = getshifts_adi(I,k); 
+
+% use Bartels-Stewart to solve AX - XB = C
+% seen as 'precise' solution
+X_ly = lyap(A, -B, -C);
+
+%% call ADI and check relative 2 norm error and norm of residual
+X_ADI = adi(A, B, C, p, q);
+rel2err_ADI = norm(X_ly - X_ADI)/norm(X_ly)
+residual_ADI = norm(A*X_ADI - X_ADI*B - C)
+
+%% call fADI and check relative 2 norm error and norm of residual
+[ZZ, DD, YY] = fadi(A,B,G,F, p,q);
+X_fADI = ZZ*DD*YY';
+rel2err_fADI = norm(X_ly - X_fADI)/norm(X_ly)
+residual_fADI = norm(A*X_fADI - X_fADI*B - C)
